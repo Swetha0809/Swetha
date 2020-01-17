760a6f8d249bc1f85f7c11d84afd59792f90f071
user <user@DESKTOP-SVCGOI8>
2020-01-17 21:06:54 +0530

    SecondCommit

--- MavenDemo/src/test/java/MavenTraining/FBLogin.java
+++ MavenDemo/src/test/java/MavenTraining/FBLogin.java
@@ -10,7 +10,7 @@ public class FBLogin {
 	System.setProperty("webdriver.chrome.driver","C:\\Users\\user\\Downloads\\Selenium Softwares\\chromedriver_win32\\chromedriver.exe");
 	WebDriver driver=new ChromeDriver();
 	driver.get("https://facebook.com");
-	driver.close();
+	
 	}
 	
 
