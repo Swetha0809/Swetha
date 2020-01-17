07e967bed6d3e9af42e4365f50202fe4efdc0598
user <user@DESKTOP-SVCGOI8>
2020-01-17 20:52:19 +0530

    FirstCommit

---
+++ GradleTraining/src/test/java/GradleTraining/GradleFBlogin.java
@@ -0,0 +1,14 @@
+package GradleTraining;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GradleFBlogin {
+	@Test
+	public void launchApp() {
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\user\\Downloads\\Selenium Softwares\\chromedriver_win32\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://facebook.com");
+	}
+}
